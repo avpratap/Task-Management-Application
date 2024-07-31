106f949d1a13a4c8e39948bc46e3b518b7795053
avpratap <vimlapratap25@gmail.com>
2024-07-31 20:54:42 +0530

    Exception handling at CreateUser method

--- my-app/backend/models/User.js
+++ my-app/backend/models/User.js
@@ -2,11 +2,16 @@ const pool = require('../config/db');
 
 const createUser = async (name, email, password) => {
   console.log("creating a new user with email: ", email)
+  try{
   const result = await pool.query(
     'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
     [name, email, password]
   );
   console.log("sucessfully created new user with email: ", email)
+}
+catch(error){
+  console.log("Exception while running ADD user query: ", error);
+}
   return result.rows[0];
 };
 
