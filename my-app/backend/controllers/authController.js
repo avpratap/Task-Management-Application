88cea69fc639ac4995552ae7614dda5701d331e1
avpratap <vimlapratap25@gmail.com>
2024-08-01 01:22:15 +0530

    added controller method

--- my-app/backend/controllers/authController.js
+++ my-app/backend/controllers/authController.js
@@ -1,5 +1,7 @@
+const pool = require('pg');
 const bcrypt = require("bcryptjs");
 const { createUser, getUserByEmail } = require('../models/User');
+
 const generateToken = require('../utils/generateToken');
 
 const register = async (req, res) => {
@@ -27,7 +29,27 @@ const login = async (req, res) => {
   res.status(200).json({ token });
 };
 
+const testController = async (req, res) => {
+  const { name, email, password } = req.body;
+  // const pool = new pool(  );
+  console.log("new registration for email, password:", email, password);
+  // const { Pool } = require('pg')
+  // require('dotenv').config()
+  
+  const pool = new Pool({
+    connectionString: "postgres://default:lxwLT5i6XVbK@ep-falling-meadow-a4opj67j-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
+  })
+
+const result = await pool.query(
+    'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
+    [name, email, password]
+  );
+
+  res.status(201).json({ message: 'User registered successfully' });
+};
+
 module.exports = {
   register,
   login,
+  testController
 };
