892d8575c85e2df3614db835032a836a02cf03b9
avpratap <vimlapratap25@gmail.com>
2024-08-01 01:41:34 +0530

    revert changes

--- my-app/backend/controllers/authController.js
+++ my-app/backend/controllers/authController.js
@@ -1,4 +1,3 @@
-const Pool = require('pg');
 const bcrypt = require("bcryptjs");
 const { createUser, getUserByEmail } = require('../models/User');
 
@@ -29,27 +28,7 @@ const login = async (req, res) => {
   res.status(200).json({ token });
 };
 
-const testController = async (req, res) => {
-  const { name, email, password } = req.body;
-  // const pool = new pool(  );
-  console.log("new registration for email, password:", email, password);
-  // const { Pool } = require('pg')
-  // require('dotenv').config()
-  
-  const pool = new Pool({
-    connectionString: "postgres://default:lxwLT5i6XVbK@ep-falling-meadow-a4opj67j-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
-  })
-
-const result = await pool.query(
-    'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
-    [name, email, password]
-  );
-
-  res.status(201).json({ message: 'User registered successfully' });
-};
-
 module.exports = {
   register,
-  login,
-  testController
+  login
 };
